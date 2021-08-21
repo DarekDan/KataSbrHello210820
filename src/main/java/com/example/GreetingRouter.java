2759ce071f7719a2c3d5c6e3b4025e95741d1e8f
Danielewski, Dariusz <dariusz.danielewski@fiserv.com>
2021-08-21 09:10:52 -0400

    Updated documentation

--- src/main/java/com/example/GreetingRouter.java
+++ src/main/java/com/example/GreetingRouter.java
@@ -13,6 +13,13 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 public class GreetingRouter {
     @Bean
     public RouterFunction<ServerResponse> route(GreetingHandler greetingHandler) {
+        /*
+        RouterFunctions.route() starts a Builder, but route(RequestPredicate, RouterFunction) returns a RouterFunction.
+        RouterFunction.and() would chain two different functions for one route.
+        RouterFunction.andRoute() "adds" additional route if the prior is not matching for the specified predicate.
+        RouterFunction.andOther() would execute secondary function if prior return no result.
+        RouterFunction.andNest() returns composite routing function.
+        */
         return RouterFunctions
                 .route(GET("/hello").and(accept(MediaType.APPLICATION_JSON)), greetingHandler::hello)
                 .andRoute(POST("/hello").and(accept(MediaType.APPLICATION_JSON)), greetingHandler::helloWithContent)
