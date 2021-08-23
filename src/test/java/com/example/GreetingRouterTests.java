7623938f266898be79cc69dfdd4ed988f3de203f
Danielewski, Dariusz <dariusz.danielewski@fiserv.com>
2021-08-23 10:10:04 -0400

    Removed redumdamt test extension @ExtendWith({SpringExtension.class}) is part of @SpringBootTest

--- src/test/java/com/example/GreetingRouterTests.java
+++ src/test/java/com/example/GreetingRouterTests.java
@@ -2,17 +2,14 @@ package com.example;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GreetingRouterTests {
 
