61a895130c856f110c53e07da404ba0ebad14f21
Danielewski, Dariusz <dariusz.danielewski@fiserv.com>
2021-09-05 20:45:33 -0500

    Added one more step in teh scenario

--- src/test/scala/GreetingSimulation.scala
+++ src/test/scala/GreetingSimulation.scala
@@ -14,12 +14,16 @@ class GreetingSimulation extends Simulation {
     .userAgentHeader("Gatling Runner")
 
   val scn = scenario("Greeting API tests") // A scenario is a chain of requests and pauses
-    .exec(http("hello")
+    .exec(http("hello-get")
       .get("/hello"))
     .pause("5", "500", TimeUnit.MILLISECONDS)
-    .exec(http("hello-reversed") // Here's an example of a POST request
-      .post("/helloReversed")
+    .exec(http("hello-post") // Here's an example of a POST request
+      .post("/hello")
       .body(StringBody("""{ "message": "Darek" }""")).asJson)
+    .pause("5", "500", TimeUnit.MILLISECONDS)
+    .exec(http("hello-post-reversed") // Here's an example of a POST request
+      .post("/helloReversed")
+      .body(StringBody("""{ "message": "Dariusz" }""")).asJson)
 
   setUp(scn.inject(rampUsers(10000).during(1.minutes)).protocols(httpProtocol))
 }
