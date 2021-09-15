446dbfe8fa2b82795db297e7afa60c24e615b1ae
Danielewski, Dariusz <dariusz.danielewski@fiserv.com>
2021-09-15 15:06:47 -0400

    Removed obsolete class decorator

--- src/main/java/com/example/service/GreetingServiceImpl.java
+++ src/main/java/com/example/service/GreetingServiceImpl.java
@@ -6,14 +6,12 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.stereotype.Service;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.validation.MapBindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.Validator;
 
-@Service
 public class GreetingServiceImpl implements GreetingService {
 
     private final Validator greetingValidator = new GreetingValidator();
